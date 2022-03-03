8dc324fe6a1d917b71bfd02d249e99f4a054d10e
jjjjjjji-hun <lingcke@naver.com>
2022-03-03 16:37:30 +0900

    서블릿 2일차

--- MyFirstWeb/src/main/java/kr/co/ictservlet/ServletCustom.java
+++ MyFirstWeb/src/main/java/kr/co/ictservlet/ServletCustom.java
@@ -1,6 +1,8 @@
 package kr.co.ictservlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,7 +36,7 @@ public class ServletCustom extends HttpServlet {
 	 * @see Servlet#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("/banana 접속시 생성된 객체는 서버 종료로 인해 파기됩니다.");
 	}
 
 	/**
@@ -47,20 +49,51 @@ public class ServletCustom extends HttpServlet {
 		// "jsp", "boot" 라는 이름으로 들어온 요소를 콘솔에 찍도록 아래 코드를 작성하고
 		// doGet메서드를 복사해서 저한테 보내주세요.
 		// 확인하는 방법은 ?jsp=값1&boot=값2를 주소 뒤에 붙입니다.
+		request.setCharacterEncoding("utf-8");
 		String jsp = request.getParameter("jsp");
 		String boot = request.getParameter("boot");
 		System.out.println(jsp + ", " + boot);
 		// "jpa"라는 이름으로 들어온 요소를 콘솔에 찍도록 추가해주세요.
 		String jpa = request.getParameter("jpa");
 		System.out.println(jpa);
+		// 리다이렉트 방식은 페이지만 이동하고 데이터는 같이 전송하지 않습니다.
+		//response.sendRedirect("http://localhost:8181/MyFirstWeb/servletForm/bananaresult.jsp");
+		// 포워딩 하기 전 request에 setAttribute()를 이용해 데이터를 저장합니다.
+		// request.setAttribute("보낼이름", 자료);
+		request.setAttribute("jsp", jsp);
+		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
+		// 상단 변수를 함께 전달하기 위해 forward를 대신 사용합니다.
+		// 목적지 주소는 localhost:포트번호/프로젝트명/이후경로
+		RequestDispatcher dp = request.getRequestDispatcher("/servletForm/bananaresult.jsp");
+		dp.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// doPost는 현재까지 배운 내용만 놓고 봤을때는 form에서 post 방식으로 제출될때 호출됩니다.
+		System.out.println("/banana 주소 post방식으로 접속함");
+		// servletForm 내부에 있던 ~~~From.jsp의 목적지는 그대로 두고시고
+		// 전송 방식만 post로 바꿔서 데이터를 날려주시고 받아서 처리해주세요.
+		// 대신 post방식으로 전송됨을  구분하기 위해서
+		// System.out.println("post방식 : " + 변수명);
+		request.setCharacterEncoding("utf-8");
+		String jsp = request.getParameter("jsp");
+		String boot = request.getParameter("boot");
+		String jpa = request.getParameter("jpa");
+		System.out.println("post방식으로 전송 : " + jsp + ", " + boot + ", " + jpa);
+		
+		// 아래 redirect방식 대신, bananaPostResult.jsp를 생성해주신 다음
+		// post방식 처리시 포워딩으로 jsp,boot, jpa변수가 화면에 표출되도록 해주세요.
+		//response.sendRedirect("http://localhost:8181/MyFirstWeb/servletForm/bananaresult.jsp");
+		request.setAttribute("jsp", jsp);
+		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
+		
+		RequestDispatcher dp = request.getRequestDispatcher("/servletForm/bananaresult.jsp");
+		dp.forward(request, response);
 	}
 
 }
--- MyFirstWeb/src/main/webapp/servletForm/bananaForm.jsp
+++ MyFirstWeb/src/main/webapp/servletForm/bananaForm.jsp
@@ -9,7 +9,7 @@
 <body>
 	<!-- 서블릿으로 보내기 위해서는 action에 서블릿 접속 주소를 복붙합니다.
 	http부터 적어넣으면 됩니다. -->
-	<form action="http://localhost:8181/MyFirstWeb/banana" method="get">
+	<form action="http://localhost:8181/MyFirstWeb/banana" method="post">
 		<!-- /banana에서 받을수 있도록 처리해주세요. input text 3개를 만들어서
 		submit 버튼을 눌렀을때 /banana의 콘솔에서 해당 자료들을 받아서 콘솔에 찍도록
 		form을 코딩한 다음 저한테 보내주세요. -->
